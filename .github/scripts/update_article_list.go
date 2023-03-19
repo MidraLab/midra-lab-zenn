991747deb8b42be56cee1d507f58eefede27e2d2
yousan <ka1357amnbpdr@gmail.com>
2023-03-19 14:14:24 +0900

    fix: actions

--- .github/scripts/update_article_list.go
+++ .github/scripts/update_article_list.go
@@ -2,6 +2,7 @@ package main
 
 func main() {
 	//todo get all notion db pages
+
 	//todo get all local md files
 	//todo find published flag is ture in local files
 	//todo compare local files with notion db pages
