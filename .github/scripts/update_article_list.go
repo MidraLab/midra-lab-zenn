4be237e126d991a3438669d2501e0695a6cc51d0
yousan <ka1357amnbpdr@gmail.com>
2023-03-19 11:11:28 +0900

    fix: add todo tasks

--- .github/scripts/update_article_list.go
+++ .github/scripts/update_article_list.go
@@ -1,5 +1,10 @@
 package main
 
 func main() {
+	//todo get all notion db pages
+	//todo get all local md files
+	//todo find published flag is ture in local files
+	//todo compare local files with notion db pages
+	//todo if local file is not in notion db pages, add it to notion db pages
 	println("Hello, world!")
 }
